33f2e8c47f23942cd482817a4cc9585b91759746
Haris Baig <harisbaig7081@gmail.com>
2024-12-18 11:33:50 +0500

    Removed Secrets

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQ_vTCa7Y-NKh2N9JprffaD1qhT0PYhUQ",
-  authDomain: "chatify-475bc.firebaseapp.com",
-  projectId: "chatify-475bc",
-  storageBucket: "chatify-475bc.firebasestorage.app",
-  messagingSenderId: "138568077346",
-  appId: "1:138568077346:web:aba180c0f1d9284e823a7b",
-  measurementId: "G-31WVHWR9EC",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
